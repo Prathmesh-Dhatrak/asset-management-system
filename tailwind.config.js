492166f014d1967df6e3ed546bc4c8b0b67131c6
Prathmesh-Dhatrak <prathmesh101dhatrak@gmail.com>
2025-02-26 12:12:00 +0530

    fix: theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 export default {
   content: ["./src/**/*.tsx", "./src/**/*.css"],
   plugins: [require('daisyui'), require("@tailwindcss/forms")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 };
